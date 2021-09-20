dda61f4a35adce03d8c985756e5a8968f04c3e1c
guswl98 <43779313+guswl98@users.noreply.github.com>
2021-09-21 01:37:34 +0900

    React, Node.js 연동

--- backend/www/routes/index.js
+++ backend/www/routes/index.js
@@ -4,6 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
+  req.send({ greeting: "Hello React.js X Node.js" });
+});
+
+router.get("/api", function (req, res, next) {
+  res.send({ greeting: "Hello React.js X Node.js" });
 });
 
 router.get("/test", function (req, res, next) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,18 @@
-import "./App.css";
+import axios from "axios";
+import "./css/App.css";
+import { useEffect } from "react";
 
 function App() {
+  const callApi = async () => {
+    axios.get("/api").then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  useEffect(() => {
+    callApi();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header"></header>
