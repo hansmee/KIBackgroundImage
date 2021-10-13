35e67aac2db1546ebd429b42b8746e55ed247939
HanSangMee <ludia1234@naver.com>
2021-10-13 21:19:07 +0900

    app.js 설정

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3002;
 
 app.get("/", (req, res) => {
   res.status(200).send("hello");
 });
 
-app.listen(3002, () => {
-  console.log("server start!");
+app.listen(port, () => {
+  console.log(`Listening to http://localhost:${port} ...`);
+  console.log("Server Start");
 });
