a480260446f4fa4ab5f6fa947de10d1d5f3e0282
HanSangMee <ludia1234@naver.com>
2021-10-18 18:48:02 +0900

    /api 기본 설정

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.status(200).send("hello");
 });
 
+app.get("/api", (req, res) => {
+  res.status(200).send("hello api");
+});
+
 app.listen(port, () => {
   console.log(`Listening to http://localhost:${port} ...`);
   console.log("Server Start");
