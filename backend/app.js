846aeb00ddc880c204983b91a343c9b31c513c4c
HanSangMee <ludia1234@naver.com>
2021-10-20 01:30:52 +0900

    코드 스타일 통일

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 3002;
+const port = 3002;
 
-app.get("/", (req, res) => {
-  res.status(200).send("hello");
+app.get('/', (req, res) => {
+  res.status(200).send('hello');
 });
 
-app.get("/api", (req, res) => {
-  res.status(200).send("hello api");
+app.get('/api', (req, res) => {
+  res.status(200).send('hello api');
 });
 
 app.listen(port, () => {
   console.log(`Listening to http://localhost:${port} ...`);
-  console.log("Server Start");
+  console.log('Server Start');
 });
