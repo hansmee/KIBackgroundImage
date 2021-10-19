efaa75c0037eeb85099b52b77ad2f06322e969a4
HanSangMee <ludia1234@naver.com>
2021-10-20 05:40:39 +0900

    디자인 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,18 +21,28 @@ export default function App() {
   };
 
   useEffect(() => {
-    callApi();
+    // callApi();
   }, []);
 
   return (
     <div className="App">
-      <div className="App-header">KIBackgroundImage</div>
-      <div className="App-body">
-        <div className="button_container">
-          <KakaoLogin setKakaoProfileImg={setKakaoProfileImg} />
-          <Instagram setInstaImgs={setInstaImgs} />
-        </div>
-        <Album kakaoProfileImg={kakaoProfileImg} instaImgs={instaImgs} />
+      <img
+        className="App_background"
+        src="https://images.unsplash.com/photo-1612820153700-a4f8795c77f4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3240&q=80"
+        alt="KIBackgroundImage"
+      />
+      <div className="App_container">
+        <header className="App-header">
+          <div className="App-title">KIBackgroundImage</div>
+          <div className="button_container">
+            <KakaoLogin setKakaoProfileImg={setKakaoProfileImg} />
+            <Instagram setInstaImgs={setInstaImgs} />
+          </div>
+        </header>
+        <main className="App-body">
+          <Album kakaoProfileImg={kakaoProfileImg} instaImgs={instaImgs} />
+          <button className="result_button">배경 이미지 추천 받기</button>
+        </main>
       </div>
     </div>
   );
