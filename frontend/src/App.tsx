1591692be818ebdc33c478ba15f9766309b3e12a
HanSangMee <ludia1234@naver.com>
2021-10-20 01:10:30 +0900

    인스타그램 버튼 분리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import './css/App.css';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import KakaoLogin from './KakaoLogin';
+import Instagram from './Instagram';
 import Album from './Album';
+import './css/App.css';
 
 export default function App() {
   const [kakaoProfileImg, setKakaoProfileImg] = useState<string>('');
@@ -26,20 +26,13 @@ export default function App() {
     callApi();
   }, []);
 
-  const clickInsta = () => {
-    const INSTAGRAM_AUTH_URL = `https://api.instagram.com/oauth/authorize/?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_INSTAGRAM_SECTRET}&redirect_uri=${process.env.REACT_APP_CALLBAK_URL}&response_type=code&scope=user_profile,user_media`;
-    window.open(INSTAGRAM_AUTH_URL, '_blank');
-  };
-
   return (
     <div className="App">
       <div className="App-header">KIBackgroundImage</div>
       <div className="App-body">
         <div className="button_container">
           <KakaoLogin setKakaoProfileImg={setKakaoProfileImg} />
-          <button className="instagram_login" onClick={clickInsta}>
-            인스타그램 로그인하기
-          </button>
+          <Instagram setInstaImgs={setInstaImgs} />
         </div>
         <Album kakaoProfileImg={kakaoProfileImg} instaImgs={instaImgs} />
       </div>
