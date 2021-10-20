af7e2bf16b6d0fe67bea856a7fa7c02893c0b848
HanSangMee <ludia1234@naver.com>
2021-10-21 03:29:46 +0900

    이미지 url 받아서 모델 결과 반환

--- backend/controllers/recommend.controller.js
+++ backend/controllers/recommend.controller.js
@@ -8,15 +8,21 @@ exports.recommend = async (req, res) => {
   //     images: feedImages,
   //   });
 
-  const child = spawn('python3', ['../model/main.py']);
+  const child = spawn('python3', [
+    '../model/main.py',
+    'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/c315.0.810.810a/s150x150/244755952_606607363816936_5060313622472449639_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=4UwabGaVUC4AX_7ATby&edm=ABfd0MgBAAAA&ccb=7-4&oh=fa9d778d537f9a2a4538775abceefc84&oe=6176DA99&_nc_sid=7bff83 https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/c315.0.810.810a/s150x150/244755952_606607363816936_5060313622472449639_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=4UwabGaVUC4AX_7ATby&edm=ABfd0MgBAAAA&ccb=7-4&oh=fa9d778d537f9a2a4538775abceefc84&oe=6176DA99&_nc_sid=7bff83',
+  ]);
   let result = '';
 
   child.stdout.on('data', (res) => {
-    result += res.toString();
+    result = res.toString();
   });
 
   child.stderr.on('data', (err) => {
     console.log(err.toString());
+    res.status(500).send({
+      error: err.toString(),
+    });
   });
 
   child.on('close', () => {
