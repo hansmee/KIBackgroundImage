d60c5ee7ac78e60038878d66b374e52c5af2282f
HanSangMee <ludia1234@naver.com>
2021-10-21 03:36:16 +0900

    추천 api 구현

--- backend/controllers/recommend.controller.js
+++ backend/controllers/recommend.controller.js
@@ -1,17 +1,10 @@
 const spawn = require('child_process').spawn;
 
 exports.recommend = async (req, res) => {
-  //   const { id } = req.params;
-  //   const { username, password } = req.body;
-  //   const feedImages = await crawler(username, password, id);
-  //   res.status(200).send({
-  //     images: feedImages,
-  //   });
+  const { images } = req.body;
+  const imgUrls = images.join(' ');
 
-  const child = spawn('python3', [
-    '../model/main.py',
-    'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/c315.0.810.810a/s150x150/244755952_606607363816936_5060313622472449639_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=4UwabGaVUC4AX_7ATby&edm=ABfd0MgBAAAA&ccb=7-4&oh=fa9d778d537f9a2a4538775abceefc84&oe=6176DA99&_nc_sid=7bff83 https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/c315.0.810.810a/s150x150/244755952_606607363816936_5060313622472449639_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=4UwabGaVUC4AX_7ATby&edm=ABfd0MgBAAAA&ccb=7-4&oh=fa9d778d537f9a2a4538775abceefc84&oe=6176DA99&_nc_sid=7bff83',
-  ]);
+  const child = spawn('python3', ['../model/main.py', imgUrls]);
   let result = '';
 
   child.stdout.on('data', (res) => {
--- backend/routes/recommend.route.js
+++ backend/routes/recommend.route.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { recommend } = require('../controllers/recommend.controller');
 
-router.get('/', recommend);
+router.post('/', recommend);
 
 module.exports = router;
